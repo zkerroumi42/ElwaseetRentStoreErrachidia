aae4b2b11230572ed60b7dc662c481f2fc297cae
zkerroumi42 <za.kerroumi42@gmail.com>
2024-12-06 16:21:20 +0100

    add datatable for list-appartement component

--- Frontend/src/app/appartements/appartements.module.ts
+++ Frontend/src/app/appartements/appartements.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ListAppartementComponent } from './list-appartement/list-appartement.component';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 const routes: Routes = [
   {
     path: '',
@@ -22,6 +28,11 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    MatTableModule,
+    MatSortModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
     RouterModule.forChild(routes),
   ],
   exports: [
@@ -29,3 +40,6 @@ const routes: Routes = [
   ]
 })
 export class AppartementsModule { }
+
+
+
