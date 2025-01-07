40fe31d8b71574ef97f9a8a6f46da7fc0a8b35e2
belqarsoussama <belqarsoussama@gmail.com>
2025-01-07 20:31:55 +0100

    modification design frontend

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';  // Importer FormsModule ici
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppartementsModule } from './appartements/appartements.module';
 import { ProprietaireComponent } from './proprietaire/proprietaire.component';
-import { HttpClientModule } from '@angular/common/http'; // Correct import for HttpClientModule
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';  // Importer HttpClientModule
 
 @NgModule({
   declarations: [
@@ -20,13 +22,17 @@ import { HttpClientModule } from '@angular/common/http'; // Correct import for H
   ],
   imports: [
     BrowserModule,
+    MatCardModule,
+    MatButtonModule,
     AppartementsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    FormsModule,
-    HttpClientModule,  // Correct placement of HttpClientModule
+    FormsModule,  // Ajouter FormsModule ici pour utiliser ngModel
+    HttpClientModule,  // Ajouter HttpClientModule pour les requêtes HTTP
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Frontend/src/app/appartements/list-appartement/appartement.service.ts
+++ Frontend/src/app/appartements/list-appartement/appartement.service.ts
@@ -13,7 +13,28 @@ export class AppartementService {
 
   // Récupérer les appartements depuis l'API
   getAppartements(): Observable<any[]> {
-    const appartements = this.http.get<any[]>(this.apiUrl);
-    return appartements; // Retourne l'Observable pour que le composant puisse s'abonner
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  // Ajouter un nouvel appartement
+  addAppartement(appartement: any): Observable<any> {
+    const proprietaireId = appartement.proprietaireId;  // Récupérer l'ID du propriétaire depuis le modèle
+    const url = `${this.apiUrl}?proprietaireId=${proprietaireId}`; // Ajouter proprietaireId dans l'URL
+    return this.http.post<any>(url, appartement); // Envoyer l'appartement dans le body
+  }
+
+  // Mettre à jour un appartement existant
+  updateAppartement(appartement: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${appartement.id}`, appartement);
+  }
+
+  // Ajouter une chambre à un appartement
+  addChambre(appartementId: number, chambre: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/${appartementId}/chambres`, chambre);
+  }
+
+  // Mettre à jour une chambre d'un appartement
+  updateChambre(appartementId: number, chambre: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${appartementId}/chambres/${chambre.id}`, chambre);
   }
 }
